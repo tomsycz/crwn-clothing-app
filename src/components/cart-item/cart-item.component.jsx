86eb9a5a1aaf12b097165d6b7a6b0b713300b503
Thomas Syczyk <tomsycz@gmail.com>
2021-04-26 10:12:01 +0100

    updating our application with reselect, replaced cart selectores with memoized version of selector

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -4,8 +4,8 @@ import "./cart-item.styles.scss";
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
   <div className="cart-item">
     <img src={imageUrl} alt="item" />
-    <div className="item-dateils">
-      <span className="name">{name}</span>
+    <div className="item-details">
+      <span className="name">{name} </span>
       <span className="price">
         {quantity} x ${price}
       </span>
