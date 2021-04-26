60949cf49faec19ad6b01c10f35c4cbcc1ee52b2
Thomas Syczyk <tomsycz@gmail.com>
2021-04-26 19:24:24 +0100

    updating stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51IkYcSIUJCZFQ1hE5qv6qDU3JWr994BR29oHToEwFRlRq8Dfm6R589M4zZrJQp5D3AAPm8TtMGAJnPZBTVoM0IV600YVcLdL0o";
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
