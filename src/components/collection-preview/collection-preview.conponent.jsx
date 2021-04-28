8d9c1abc36c4e61c3cc68385de479b74944512fe
Thomas Syczyk <tomsycz@gmail.com>
2021-04-28 10:39:47 +0100

    added links to collection preview titles and added spinner HOC

--- src/components/collection-preview/collection-preview.conponent.jsx
+++ src/components/collection-preview/collection-preview.conponent.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./collection-preview.styles.scss";
+import { withRouter } from "react-router-dom";
 
 import CollectionItem from "../collection-item/collection-item.component";
 
-function CollectionPreview({ title, items }) {
+function CollectionPreview({ title, items, routeName, match, history }) {
   return (
     <div className="collection-preview">
-      <h1>{title.toUpperCase()}</h1>
+      <h1
+        className="link-to-collection"
+        onClick={() => {
+          history.push(`${match.url}/${routeName}`);
+        }}
+      >
+        {title.toUpperCase()}
+      </h1>
       <div className="preview">
         {items
           .filter((item, idx) => idx < 4)
@@ -18,4 +26,4 @@ function CollectionPreview({ title, items }) {
   );
 }
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,17 +2,26 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 
-import CollectionOverview from "./../../components/collections-overview/collections-overview.component";
-import CollectionPage from "../collection/collection.component";
-
 import {
   firestore,
   convertCollectionsSnapshotToMap,
 } from "../../firebase/firebase.utils";
 
+//HOC -HIGHER ORDER COMPONENT
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
+
+import CollectionOverview from "./../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
+
 import { updateCollections } from "../../redux/shop/shop.actions";
 
+const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends Component {
+  state = {
+    loading: true,
+  };
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
@@ -22,16 +31,26 @@ class ShopPage extends Component {
     this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
+      this.setState({ loading: false });
     });
   }
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={props => (
+            <CollectionOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={props => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
         />
       </div>
     );
