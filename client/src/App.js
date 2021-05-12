1f9be8cb80765a2ce9d114b9bf721e6fa8611645
Thomas Syczyk <tomsycz@gmail.com>
2021-05-12 11:27:05 +0100

    added media queries to the homepage, header and shop elements

--- client/src/App.js
+++ client/src/App.js
@@ -3,26 +3,25 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import "./App.css";
-
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import CheckoutPage from "./pages/checkout/checkout.component";
-
 import Header from "./components/header/header.component";
+
+import { GlobalStyle } from "./global.styles";
 
 import { selectCurrentUser } from "./redux/user/user.selectors";
 import { checkUserSession } from "./redux/user/user.actions";
 
 const App = ({ checkUserSession, currentUser }) => {
-  
   useEffect(() => {
     checkUserSession();
   }, [checkUserSession]);
 
   return (
     <div>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
